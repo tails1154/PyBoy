fa4cb18b4bce40cd841d8ae34673b2fb28268aec
Timothy <152324947+tails1154@users.noreply.github.com>
2025-07-16 19:14:07 -0500

    flask api frfr

--- pyboy/core/serial.py
+++ pyboy/core/serial.py
@@ -1,16 +1,13 @@
-#
-# License: See LICENSE.md file
-# GitHub: https://github.com/Baekalfen/PyBoy
-#
-
+from flask import Flask, request, jsonify
 from pyboy.utils import MAX_CYCLES
+
+app = Flask(__name__)
 
 CYCLES_8192HZ = 128
 
-
 class Serial:
     def __init__(self):
-        self.SB = 0xFF  # Always 0xFF for a disconnected link cable
+        self.SB = 0xFF
         self.SC = 0
         self.transfer_enabled = 0
         self.internal_clock = 0
@@ -18,61 +15,72 @@ class Serial:
         self.last_cycles = 0
         self.clock = 0
         self.clock_target = MAX_CYCLES
+        self.received_data = []
 
     def set_SB(self, value):
-        # Always 0xFF when cable is disconnected. Connecting is not implemented yet.
-        self.SB = 0xFF
+        self.SB = value
 
-    def set_SC(self, value):  # cgb, double_speed
+    def get_SB(self):
+        return self.SB
+
+    def set_SC(self, value):
         self.SC = value
         self.transfer_enabled = self.SC & 0x80
-        # TODO:
-        # if cgb and (self.SC & 0b10): # High speed transfer
-        #     self.double_speed = ...
-        self.internal_clock = self.SC & 1  # 0: external, 1: internal
+        self.internal_clock = self.SC & 1
         if self.internal_clock:
             self.clock_target = self.clock + 8 * CYCLES_8192HZ
         else:
-            # Will never complete, as there is no connection
-            self.transfer_enabled = 0  # Technically it is enabled, but no reason to track it.
-            self.clock_target = MAX_CYCLES
-        self._cycles_to_interrupt = self.clock_target - self.clock
-
-    def tick(self, _cycles):
-        cycles = _cycles - self.last_cycles
-        if cycles == 0:
-            return False
-        self.last_cycles = _cycles
-
-        self.clock += cycles
-
-        interrupt = False
-        if self.transfer_enabled and self.clock >= self.clock_target:
-            self.SC &= 0x80
             self.transfer_enabled = 0
-            # self._cycles_to_interrupt = MAX_CYCLES
             self.clock_target = MAX_CYCLES
-            interrupt = True
-
         self._cycles_to_interrupt = self.clock_target - self.clock
-        return interrupt
 
-    def save_state(self, f):
-        f.write(self.SB)
-        f.write(self.SC)
-        f.write(self.transfer_enabled)
-        f.write(self.internal_clock)
-        f.write_64bit(self.last_cycles)
-        f.write_64bit(self._cycles_to_interrupt)
-        f.write_64bit(self.clock)
-        f.write_64bit(self.clock_target)
+    def get_SC(self):
+        return self.SC
 
-    def load_state(self, f, state_version):
-        self.SB = f.read()
-        self.SC = f.read()
-        self.transfer_enabled = f.read()
-        self.internal_clock = f.read()
-        self.last_cycles = f.read_64bit()
-        self._cycles_to_interrupt = f.read_64bit()
-        self.clock = f.read_64bit()
-        self.clock_target = f.read_64bit()
+    def send_data(self, data):
+        self.received_data.append(data)
+        self.set_SB(data)
+
+    def receive_data(self):
+        if self.received_data:
+            return self.received_data.pop(0)
+        else:
+            return None
+
+serial = Serial()
+
+@app.route('/serial/send', methods=['POST'])
+def send_data():
+    data = request.json.get('data')
+    if data is None:
+        return jsonify({'error': 'No data provided'}), 400
+    serial.send_data(data)
+    return jsonify({'status': 'ok', 'sent': data})
+
+@app.route('/serial/receive', methods=['GET'])
+def receive_data():
+    data = serial.receive_data()
+    if data is None:
+        return jsonify({'status': 'empty'})
+    return jsonify({'status': 'ok', 'data': data})
+
+@app.route('/serial/SB', methods=['GET', 'POST'])
+def sb():
+    if request.method == 'POST':
+        value = request.json.get('value')
+        serial.set_SB(value)
+        return jsonify({'status': 'ok', 'SB': value})
+    else:
+        return jsonify({'SB': serial.get_SB()})
+
+@app.route('/serial/SC', methods=['GET', 'POST'])
+def sc():
+    if request.method == 'POST':
+        value = request.json.get('value')
+        serial.set_SC(value)
+        return jsonify({'status': 'ok', 'SC': value})
+    else:
+        return jsonify({'SC': serial.get_SC()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
